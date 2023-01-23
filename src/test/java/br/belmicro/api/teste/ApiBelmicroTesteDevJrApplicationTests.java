10ef42bc022af4ff6f9c6006a5652a095e58c6fa
igor-leao <igorleao.eng@gmail.com>
2023-01-23 12:02:23 -0300

    testes de integracao

--- src/test/java/br/belmicro/api/teste/ApiBelmicroTesteDevJrApplicationTests.java
+++ src/test/java/br/belmicro/api/teste/ApiBelmicroTesteDevJrApplicationTests.java
@@ -1,13 +1,40 @@
 package br.belmicro.api.teste;
 
+import br.belmicro.api.teste.modelo.Produto;
+import br.belmicro.api.teste.service.ProdutoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import javax.validation.ConstraintViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ApiBelmicroTesteDevJrApplicationTests {
 
+	@Autowired
+	ProdutoService produtoService;
 	@Test
-	void contextLoads() {
+	void testeProdutoCadastradoComSucesso() {
+
+		//cenario
+		Produto produtoTeste = new Produto();
+		produtoTeste.setNome("headset hiperX Cloud Core");
+		produtoTeste.setValor(650L);
+		//acão
+		produtoService.create(produtoTeste);
+		// validacão
+		assertEquals(produtoTeste.getId().toString().isEmpty(), false, "temos um id válido");
+		assertEquals(produtoTeste.getNome(), "headset hiperX Cloud Core", "temos um nome válido");
 	}
+
+	//	@Test
+//	public void testarCadastroProdutoComCamposFaltantes() {
+//		final Produto produtoTeste = new Produto();
+//		produtoTeste.setNome(null);
+//
+//		assertThrows(ConstraintViolationException.class, () -> produtoService.create(produtoTeste));
+//
+//	}
 
 }
